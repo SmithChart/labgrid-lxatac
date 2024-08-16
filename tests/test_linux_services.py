639c7ebb1cd7608abcbf326276219b907aa686c1
Chris Fiege <cfi@pengutronix.de>
2024-08-16 15:46:26 +0200

    tests/linux_services: Check boot log contains known errors only
    With this test we make sure that the log from the current boot
    only contains /knowns/ errors.

---
+++ tests/test_linux_services.py
@@ -0,0 +1,37 @@
+import json
+import re
+
+
+def test_journal_warnings(shell):
+    stdout = shell.run_check(
+        "journalctl -b -k --output json --prio 4 --output-fields=MESSAGE | cat"
+    )  # use cat to avoid colors
+    entries = []
+    for line in stdout:
+        entries.append(json.loads(line))
+
+    whitelist = [
+        r"spi_stm32 44009000\.spi: failed to request tx dma channel",
+        r"spi_stm32 44009000\.spi: failed to request rx dma channel",
+        r"clk: failed to reparent ethck_k to pll4_p: -22",
+        r"stm32_rtc 5c004000\.rtc: Date\/Time must be initialized",
+        r"File \/var\/log\/journal\/.*\/system.journal corrupted or uncleanly shut down, renaming and replacing.",
+        r"stm32-dwmac 5800a000\.ethernet switch: Adding VLAN ID 0 is not supported",
+        # The following messages can be present if the USB stick is connected:
+        r"sd 0:0:0:0: \[sda\] No Caching mode page found",
+        r"sd 0:0:0:0: \[sda\] Assuming drive cache: write through",
+        r"block sda: the capability attribute has been deprecated.",
+    ]
+
+    whitelist = [re.compile(r) for r in whitelist]
+
+    remaining = []
+
+    for entry in entries:
+        for whitelist_entry in whitelist:
+            if whitelist_entry.match(entry["MESSAGE"]):
+                break
+        else:
+            remaining.append(entry["MESSAGE"])
+
+    assert remaining == []
